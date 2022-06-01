bb1b4cdae4bdce87a8986137f860c28c9bd57120
Bobby Bates <robertfbatesjr@gmail.com>
2022-05-31 21:10:39 -0400

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -9,11 +9,19 @@ console.log(user);
 
 function App() {
   return (
-    <div>
+    <>
       <NavBar />
-      <Home />
-      <About />
-    </div>
+      <Home 
+        name={user.name}
+        city={user.city}
+        color={user.color}
+      />
+      <About
+        bio={user.bio}
+        github={user.links.github}
+        linkedin={user.links.linkedin}
+      />
+    </>
   );
 }
 
